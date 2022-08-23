b089a8f9b40ce51cd4b3feee97dbfe95a3fbb5c8
Elgin88 <elgin-88@mail.ru>
2022-08-23 21:09:52 +0800

    1

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Enemy _enemy;    
+    [SerializeField] private float _delay;
+    [SerializeField] private float _duration;
+
+    private SpawnPoint [] _spawnPoints;
+    private float _timeAfterLastSpawn;    
+    private SpawnPoint _currentSpawnPoint; 
+    
+    private void Start()
+    {
+        _spawnPoints = GetComponentsInChildren<SpawnPoint>();
+        _timeAfterLastSpawn = _delay;        
+        StartCoroutine(SpawnEnemy());
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        while (true)
+        {
+            _timeAfterLastSpawn += Time.deltaTime;
+            _currentSpawnPoint = _spawnPoints[Random.Range(0, _spawnPoints.Length)];
+
+            if (_timeAfterLastSpawn > _delay)
+            {
+                InstantiateEnemy(_enemy);
+                _timeAfterLastSpawn = 0;                
+            }
+
+            yield return null;
+        }        
+    }
+
+    private void InstantiateEnemy(Enemy enemy)
+    {
+        Instantiate(enemy, _currentSpawnPoint.transform.position, Quaternion.identity);
+    }
+}
